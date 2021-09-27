8f801e379a97cd66c407926a63a73e246bb54a67
garrettkatz <garrett.katz@gmail.com>
2021-09-27 16:41:08 -0400

    improved make_tau and started constructor.run prototype

--- constructor.py
+++ constructor.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 class Constructor:
-    def __init__(self, alg, max_actions):
+    def __init__(self, alg, max_actions, scramble, γ, φ_threshold):
         self.alg = alg
         self.max_actions = max_actions
+        self.scramble = scramble
+        self.γ = γ
+        self.φ_threshold = φ_threshold
+        self.φ_history = [0.]
         self.num_incs = 0
 
     def make_ω(self, mdb, r_, s_):
@@ -12,10 +16,10 @@ class Constructor:
             for k in np.flatnonzero(mdb.prototypes[r_[n]] != s_[n])]
 
     def make_τ(self, mdb, s):
+        queries = [mdb.query(s[t]) for t in range(len(s))]
         return [(t,r)
-            for t in range(len(s))
-            for r in np.flatnonzero((s[t] == mdb.prototypes).all(axis=1))
-            if self.alg.max_depth + t + mdb.costs[r] <= self.max_actions]
+            for t,r in enumerate(queries)
+            if r != None and self.alg.max_depth + t + mdb.costs[r] <= self.max_actions]
 
     def incorporate(self, mdb, s, a):
         ϕ = True
@@ -42,9 +46,22 @@ class Constructor:
         self.num_incs += 1
         return ϕ
 
+    def run(self, mdb):
+        φ = self.φ_history[self.num_incs]
+        while φ < self.φ_threshold:
+            s, a = self.scramble(self.max_actions - self.alg.max_depth)
+            ϕ = self.incorporate(mdb, s, a)
+            # if ϕ: print(f"{self.num_incs}: φ = {φ}")
+            print(f"{self.num_incs}: φ = {φ}")
+            φ = self.γ * φ + (1. - self.γ) * int(ϕ)
+            self.φ_history.append(φ)
+
 if __name__ == "__main__":
 
     import numpy as np
+
+    γ = 0.9
+    φ_threshold = 0.9999
 
     max_depth = 1
     max_actions = 20
@@ -58,25 +75,70 @@ if __name__ == "__main__":
     bfs_tree = SearchTree(domain, max_depth)
 
     from macro_database import MacroDatabase
-    mdb = MacroDatabase(domain, 2)
+    mdb = MacroDatabase(domain, 3)
     mdb.add_rule(solved, (), 0, added=-1)
     for w in range(domain.state_size()): mdb.disable(0, w, tamed=-1)
 
     from algorithm import Algorithm
     alg = Algorithm(domain, bfs_tree, max_depth, color_neutral)
 
-    cons = Constructor(alg, max_actions)
+    scramble = lambda _: None # placeholder
+    cons = Constructor(alg, max_actions, scramble, γ, φ_threshold)
     ϕ = cons.incorporate(mdb, [solved], [])
     assert ϕ
+    assert mdb.num_rules == 1
 
     # within max depth
     s, a = [domain.perform((0,1,1), solved), solved], [(0,1,3)]
     ϕ = cons.incorporate(mdb, s, a)
     assert ϕ
+    assert mdb.num_rules == 1
 
     # outside max depth
     a = [(0,1,1), (1,1,1), (2,1,1)]
     s = [solved] + domain.intermediate_states(a, solved)
     ϕ = cons.incorporate(mdb, s[::-1], domain.reverse(a))
     assert not ϕ
+    assert mdb.num_rules == 2
+
+    # outside max depth with intermediate tau endpoint
+    for r in range(mdb.num_rules):
+        for w in range(domain.state_size()):
+            mdb.disable(r, w)
+    a = [(0,1,1), (1,1,1), (2,1,1), (1,1,1), (0,1,1), (1,1,1)]
+    s = [solved] + domain.intermediate_states(a, solved)
+    ϕ = cons.incorporate(mdb, s[::-1], domain.reverse(a))
+    assert not ϕ
+    assert mdb.num_rules == 3
+
+    #### run constructor to convergence on mini cube
+    cube_size = 2
+    valid_actions = (
+        (0,1,1), (0,1,2), (0,1,3),
+        (1,1,2), 
+    )
+    tree_depth = 11
+
+    domain = CubeDomain(cube_size, valid_actions)
+    tree = SearchTree(domain, tree_depth)
+    assert tree.depth() == tree_depth
+    
+    all_states = tree.states_rooted_at(solved)
+    optimal_paths = tuple(map(tuple, map(domain.reverse, tree.paths()))) # from state to solved
+    all_probs = list(zip(all_states, optimal_paths))
+    
+    def scramble(num_actions):
+        idx = np.random.choice(len(all_probs))
+        a = optimal_paths[idx]
+        s = [all_states[idx]] + domain.intermediate_states(a, all_states[idx])
+        return s, a
+
+    mdb = MacroDatabase(domain, len(all_probs))
+    mdb.add_rule(solved, (), 0, added=-1)
+    for w in range(domain.state_size()): mdb.disable(0, w, tamed=-1)
+
+    alg = Algorithm(domain, bfs_tree, max_depth, color_neutral)
+    cons = Constructor(alg, max_actions, scramble, γ, φ_threshold)
+
+    cons.run(mdb)
 
