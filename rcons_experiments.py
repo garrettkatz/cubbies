0efc9370425524e1764e4089d1e7189af7c36052
garrettkatz <garrett.katz@gmail.com>
2021-12-10 22:05:27 -0500

    added confirmation to rce

--- rcons_experiments.py
+++ rcons_experiments.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     import pickle as pk
     from cube import CubeDomain
 
-    do_cons = True
+    do_cons = False
     showresults = False
-    confirm = False
+    confirm = True
 
     γ = 0.99
     ema_threshold = 1.1
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     dump_base = "N%d%s_D%d_M%d_cn%d" % (
         cube_size, cube_str, tree_depth, max_depth, color_neutral)
 
-    if do_cons:
+    if do_cons or confirm:
 
         print("making states...")
         from tree import SearchTree
@@ -44,6 +44,8 @@ if __name__ == "__main__":
 
         bfs_tree = SearchTree(domain, max_depth)
         alg = Algorithm(domain, bfs_tree, max_depth, color_neutral)
+
+    if do_cons:
 
         if not os.path.exists(dump_dir): os.mkdir(dump_dir)
 
@@ -97,6 +99,26 @@ if __name__ == "__main__":
 
             with open(os.path.join(dump_dir, dump_base + f"_{rep}.pkl"), "wb") as f:
                 pk.dump((mdb, con, checks, success_rates, total_time), f)
+
+    if confirm:
+
+        rep = 0
+        with open(os.path.join(dump_dir, dump_base + f"_{rep}.pkl"), "rb") as f:
+            (mdb, con, checks, success_rates, total_time) = pk.load(f)
+
+        start = perf_counter()
+        correct = True
+        success_count = 0
+        for t in range(tree.size()):
+            if t % 1000 == 0: print(f"{t} of {tree.size()}")
+            state = domain.solved_state()[tree.permutations()[t]]
+            success, _, _, _ = alg.run(max_actions, mdb, state)
+            correct = correct and success
+            success_count = success_count + success
+
+        success_rate = success_count / tree.size() if exhaust else 1 - 1/first_fail
+        total_time = perf_counter() - start
+        print(f"{total_time:.2f}s, {mdb.num_rules} rules")
 
     if showresults:
 
